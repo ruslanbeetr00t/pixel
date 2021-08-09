e394eac6e8f22707792eaf38f2f748c0b1703e81
mx <https://github.com/ruslanbeetr00t/pixel.git>
2021-08-09 22:53:53 +0300

    add commit 5

--- api/__init__.py
+++ api/__init__.py
@@ -2,20 +2,29 @@ import json
 
 import requests
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+    'Authorization': '563492ad6f91700001000001786b9e4bb4bb4b74900164d6c9818e47'
+}
+
 
 def search_info():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
-        'Authorization': '563492ad6f91700001000001786b9e4bb4bb4b74900164d6c9818e47'
-    }
-    search = input('Введіть що саме ви шукаету:-') # наприклад opossum or other animals or fish
+
+    search = input('Введіть що саме ви шукаету:-')  # наприклад opossum or other animals or fish
     page = int(input('Введіть сторінку по якій вести пошук:-'))  # коректно не працюе
     web_url = f"https://api.pexels.com/v1/search?query={search}&per_page={page}"
-    responce = requests.get(web_url, headers=headers)
+    responce = requests.get(url=web_url, headers=headers)
     if responce.status_code == 200:
-        res = responce.json()['photos'][0]['url']
-        with open('animals_1.json', 'w', encoding='utf-8') as file_json:
-            json.dump(res, file_json, ensure_ascii=False, indent=4)
+        # res = responce.json()['photos'][0]['url']
+        with open('animals_2.json', 'w', encoding='utf-8') as file_json:
+            json.dump(responce.json(), file_json, ensure_ascii=False, indent=4)
+
+    with open('animals_2.json', 'r', encoding='utf-8') as file_json:
+        text = json.load(file_json)
+
+        for txt in text['photos']:
+            print(txt['url'])
+
 
 
 
